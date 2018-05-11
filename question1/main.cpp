24c24e4202bebe463484dfcd458ae6358e898a36
Ivan Chan <ivan@ivanplex.com>
2018-05-11 01:56:14 +0100

    Updated Question1 equation

--- question1/main.cpp
+++ question1/main.cpp
@@ -8,21 +8,20 @@ int main ()
 {
 	int variable = 5;
 
-	//Equation
-	// ADD<
-	//   	VAR,
-	//   	MULTIPLY<
-	// 	  	MINUS<
-	// 	  		VAR,
-	// 	  		LITERALINTEGER<2>
-	// 	  	>,
-	// 	  	MINUS<
-	// 	  		VAR,
-	// 	  		LITERALINTEGER<3>
-	// 	  	>
-	// 	>
-	// > expr;
-	DIVIDE<VAR,LITERALINTEGER<0>> expr;
+	Equation
+	ADD<
+	  	VAR,
+	  	MULTIPLY<
+		  	MINUS<
+		  		VAR,
+		  		LITERALINTEGER<2>
+		  	>,
+		  	MINUS<
+		  		VAR,
+		  		LITERALINTEGER<3>
+		  	>
+		>
+	> expr;
 
 
 	cout << expr.eval(variable);
